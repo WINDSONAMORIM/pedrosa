809b35c2fb9c5e4ab611654e181b3e1aab2b8259
windsonamorim <windson.amorim@gmail.com>
2023-07-16 23:06:04 -0300

    feat: exclui imagem

--- src/components/wrapperContent/index.tsx
+++ src/components/wrapperContent/index.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
-const bg = require('../../assets/images/logo.jpg')
 
 export const WrapperContent = styled.main`
-    width: 100vw;
-    height: 100vh;
-`
+  width: 100vw;
+  height: 100vh;
+`;
